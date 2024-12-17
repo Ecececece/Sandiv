ab9df369ff016b2af0dbfbf97ae2cb5b648281c6
Ecececece <starecex123@gmail.com>
2024-12-17 02:56:39 -0800

    Sandiv 0.30
    Sepete Ekle butonları eklendi.
    Tatlı Sandivler eklendi.
    Loading Ekranı Düzenlendi.

--- src/app/SaltySandivs/page.tsx
+++ src/app/SaltySandivs/page.tsx
@@ -1,131 +1,171 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 
 type Sandiv = {
-    name: string;
-    bread: string;
-    ingredients: string[];
-    cheese: string[] | "none";
-    sauce: string[] | "none";
+  name: string;
+  bread: string;
+  ingredients: string[];
+  cheese: string[] | "none";
+  sauce: string[] | "none";
 };
 
-export default function Sal() {
-    const [sandivs, setSandivs] = useState<Sandiv[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+export default function SaltySandivs() {
+  const [sandivs, setSandivs] = useState<Sandiv[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        async function fetchSandivs() {
-            try {
-                const response = await fetch('/saltysandivs.json');
-                if (!response.ok) throw new Error('Veri yüklenemedi');
+  const [dots, setDots] = useState("");
 
-                const data = await response.json();
+  useEffect(() => {
+    async function fetchSandivs() {
+      try {
+        const response = await fetch("/saltysandivs.json");
+        if (!response.ok) throw new Error("Veri yüklenemedi");
 
-                // Tip kontrolü ekleniyor
-                if (Array.isArray(data.Sandivs)) {
-                    setSandivs(data.Sandivs);
-                } else {
-                    setSandivs([]);
-                }
+        const data = await response.json();
 
-                setLoading(false);
-            } catch (err) {
-                setError(err instanceof Error ? err.message : "Bilinmeyen hata");
-                setLoading(false);
-            }
+        // Tip kontrolü ekleniyor
+        if (Array.isArray(data.Sandivs)) {
+          setSandivs(data.Sandivs);
+        } else {
+          setSandivs([]);
         }
 
-        fetchSandivs();
-    }, []);
+        setLoading(false);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "Bilinmeyen hata");
+        setLoading(false);
+      }
 
-    if (loading) return <div>Yükleniyor...</div>;
+      const interval = setInterval(() => {
+        setDots((prev) => (prev === "..." ? "" : prev + ".")); // Dots sırasını değiştir
+      }, 500); // Her 500ms'de bir güncelle
+      return () => clearInterval(interval);
+    }
 
-    if (error) return <div>Hata: {error}</div>;
+    fetchSandivs();
+  }, []);
 
+  if (loading){
     return (
-        <div className="flex flex-col">
-            <div className="flex justify-center gap-20 m-4">
-                <a href="/SaltySandivs" className="option-button">
-                    <div className="sandiv-button-shadow"></div>
-                    <div className="absolute">Tuzlu Sandivs</div>
-                </a>
-
-                <a href="/SweetSandivs" className="option-button">
-                    <div className="sandiv-button-shadow"></div>
-                    <div className="absolute">Tatlı Sandivs</div>
-                </a>
-
-                <a href="/SaltySandivs" className="option-button">
-                    <div className="sandiv-button-shadow"></div>
-                    <div className="absolute">Kendi Sandivin</div>
-                </a>
-            </div>
-
-            <div className="sandivs-div" id="scrollable">
-                {sandivs.length > 0 ? (
-                    sandivs.map((sandiv, index) => (
-                        <div key={index} className="sandiv">
-                            <div className="sandivShadow">
-                                <div className="sandivName">{sandiv.name}</div>
-                                <div className="sandivIMG">
-                                    <div className="flex justify-center ml-24">
-                                        {/* Ekmek resmi */}
-                                        <img
-                                            src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/bread/salty/${sandiv.bread}.png?updatedAt=1733917749146`}
-                                            className="bread"
-                                        />
-                                        {/* Tuzlu malzemeler */}
-                                        {sandiv.ingredients.map((ingredient, idx) => (
-                                            <img
-                                                key={idx}
-                                                src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/salty/${ingredient}.png?updatedAt=1733917744759`}
-                                                className="ingredient"
-                                                style={{
-                                                    bottom: `${-180 + idx * 10}px`,
-                                                }}
-                                            />
-                                        ))}
-                                        {/* Peynir resimleri */}
-                                        {sandiv.cheese !== "none" && sandiv.cheese.map((cheese, cheeseidx) => (
-                                            <img
-                                                key={cheeseidx}
-                                                src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/cheese/${cheese}.png?updatedAt=1733917747096`}
-                                                className="ingredient"
-                                                style={{
-                                                    bottom: `${-180 + (sandiv.ingredients.length + sandiv.sauce.length + cheeseidx) * 10}px`
-                                                }}
-                                            />
-                                        ))}
-                                        {/* Sos resimleri */}
-                                        {sandiv.sauce !== "none" && sandiv.sauce.map((sauce, sauceidx) => (
-                                            <img
-                                                key={sauceidx}
-                                                src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/sauce/${sauce}.png?updatedAt=1733917737445`}
-                                                className="ingredient"
-                                                style={{
-                                                    bottom: `${-180 + (sandiv.ingredients.length + sauceidx + sandiv.cheese.length) * 10}px`,
-                                                }}
-                                            />
-                                        ))}
-                                        {/* Son bir ekmek resmini ekliyoruz */}
-                                        <img
-                                            src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/bread/salty/${sandiv.bread}.png?updatedAt=1733917749146`}
-                                            className="bread"
-                                            style={{
-                                                bottom: `${-180 + (sandiv.ingredients.length + sandiv.sauce.length + sandiv.cheese.length) * 10}px`,
-                                            }}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <div>Veri bulunamadı.</div>
-                )}
-            </div>
+        <div className="loading">
+            Yükleniyor{dots}
         </div>
-    );
+    )
+  }
+
+  if (error) return <div>Hata: {error}</div>;
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-center gap-20 m-4">
+        <a href="/SaltySandivs" className="option-button">
+          <div className="sandiv-button-shadow"></div>
+          <div className="absolute">Tuzlu Sandivs</div>
+        </a>
+
+        <a href="/SweetSandivs" className="option-button">
+          <div className="sandiv-button-shadow"></div>
+          <div className="absolute">Tatlı Sandivs</div>
+        </a>
+
+        <a href="/SaltySandivs" className="option-button">
+          <div className="sandiv-button-shadow"></div>
+          <div className="absolute">Kendi Sandivin</div>
+        </a>
+      </div>
+
+      <div className="sandivs-div" id="scrollable">
+        {sandivs.length > 0 ? (
+          sandivs.map((sandiv, index) => (
+            <div key={index} className="sandiv">
+              <div className="sandivShadow">
+                <div className="sandivIMG">
+                  <div className="flex justify-center ml-24">
+                    {/* Ekmek resmi */}
+                    <img
+                      src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/bread/salty/${sandiv.bread}.png?updatedAt=1733917749146`}
+                      className="bread"
+                    />
+                    {/* Tuzlu malzemeler */}
+                    {sandiv.ingredients.map((ingredient, idx) => (
+                      <img
+                        key={idx}
+                        src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/salty/${ingredient}.png?updatedAt=1733917744759`}
+                        className="ingredient"
+                        style={{
+                          bottom: `${-180 + idx * 10}px`,
+                        }}
+                      />
+                    ))}
+                    {/* Peynir resimleri */}
+                    {sandiv.cheese !== "none" &&
+                      sandiv.cheese.map((cheese, cheeseidx) => (
+                        <img
+                          key={cheeseidx}
+                          src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/cheese/${cheese}.png?updatedAt=1733917747096`}
+                          className="ingredient"
+                          style={{
+                            bottom: `${
+                              -180 +
+                              (sandiv.ingredients.length +
+                                sandiv.sauce.length +
+                                cheeseidx) *
+                                10
+                            }px`,
+                          }}
+                        />
+                      ))}
+                    {/* Sos resimleri */}
+                    {sandiv.sauce !== "none" &&
+                      sandiv.sauce.map((sauce, sauceidx) => (
+                        <img
+                          key={sauceidx}
+                          src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/sauce/${sauce}.png?updatedAt=1733917737445`}
+                          className="ingredient"
+                          style={{
+                            bottom: `${
+                              -180 +
+                              (sandiv.ingredients.length +
+                                sauceidx +
+                                sandiv.cheese.length) *
+                                10
+                            }px`,
+                          }}
+                        />
+                      ))}
+                    {/* Son bir ekmek resmini ekliyoruz */}
+                    <img
+                      src={`https://ik.imagekit.io/zvxotlby9c/malzemeler/bread/salty/${sandiv.bread}.png?updatedAt=1733917749146`}
+                      className="bread"
+                      style={{
+                        bottom: `${
+                          -180 +
+                          (sandiv.ingredients.length +
+                            sandiv.sauce.length +
+                            sandiv.cheese.length) *
+                            10
+                        }px`,
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex justify-center relative -right-32">
+                  <div className="sandivName">{sandiv.name}</div>
+                  <a className="sepetButton" onClick={() => console.log(sandiv.name)}>
+                    <div className="sandiv-button-shadow"></div>
+                    <div className="absolute">Sepete Ekle</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>Veri bulunamadı.</div>
+        )}
+      </div>
+    </div>
+  );
 }
